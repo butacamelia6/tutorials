d70c5ca0436fc130bbbd59a3809dec988d098896
Camelia Buta <butacamelia6@gmail.com>
2024-03-22 18:41:36 +0200

    a part of the testing

---
+++ cypress/e2e/clip.cy.ts
@@ -0,0 +1,14 @@
+describe('Clip', () => {
+  it('should play clip', () => {
+    cy.visit('/');
+    cy.get('clips-list > .grid a:first').click();
+    cy.get('.video-js').click();
+    cy.wait(3000);
+    cy.get('.video-js').click();
+    cy.get('.vjs-play-progress').invoke('width').should('gte', 0);
+  });
+});
+
+// cy.wait(3000)
+// cy.get('.video-js > video').click()
+// cy.get('.vjs-play-progress').invoke('width').should('gte', 0)
--- src/app/shared/tabs-container/tabs-container.component.spec.ts
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -1,21 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
 import { TabsContainerComponent } from './tabs-container.component';
+import { TabComponent } from '../tab/tab.component';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `
+    <app-tabs-container>
+      <app-tab tabTitle="Tab1">Tab1</app-tab>
+      <app-tab tabTitle="Tab2">Tab2</app-tab>
+    </app-tabs-container>
+  `,
+})
+class TestHostComonent {}
 
 describe('TabsContainerComponent', () => {
-  let component: TabsContainerComponent;
-  let fixture: ComponentFixture<TabsContainerComponent>;
+  let component: TestHostComonent;
+  let fixture: ComponentFixture<TestHostComonent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TabsContainerComponent]
+      declarations: [TabsContainerComponent, TabComponent, TestHostComonent],
     });
-    fixture = TestBed.createComponent(TabsContainerComponent);
+    fixture = TestBed.createComponent(TestHostComonent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have two tabs', () => {
+    const tabs = fixture.debugElement.queryAll(By.css('li'));
+    const containerComponent = fixture.debugElement.query(
+      By.directive(TabsContainerComponent)
+    );
+
+    const tabsProp = containerComponent.componentInstance.tabs;
+
+    expect(tabs.length).withContext('Tabs did not render').toBe(2);
+    expect(tabsProp.length)
+      .withContext('Could not grab component property')
+      .toBe(2);
   });
 });
